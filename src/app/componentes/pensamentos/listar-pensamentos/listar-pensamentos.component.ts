729674595b2a260ae4938e234060e1810d4a022a
Leandro da Silva Soares <3973@ucdb.br>
2023-06-07 10:47:06 -0400

    Implementando a Interface Pensamento

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamentos',
@@ -7,23 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentosComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: "Aprendendo Angular",
-      autoria: "Soares Leandro",
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: "Utilizando Diretivas. Lorem: Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.",
-      autoria: "Fulano",
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: "Implementando Comunicação entre Componentes",
-      autoria: "Ciclano",
-      modelo: 'modelo3'
-    }
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() { }
 
