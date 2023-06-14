285cd0bdc9125ee3020d6965e4bfeb49851650be
Leandro da Silva Soares <3973@ucdb.br>
2023-06-14 10:31:29 -0400

    Componente Excluir

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -19,4 +19,14 @@ export class PensamentoService {
   cadastrar(pensamento: Pensamento): Observable<Pensamento>{
     return this.http.post<Pensamento>(this.API, pensamento);
   }
+
+  excluir(id: number): Observable<Pensamento>{
+    const url = `${this.API}/${id}`;
+    return this.http.delete<Pensamento>(url);
+  }
+
+  bucarPorId(id: number): Observable<Pensamento>{
+    const url = `${this.API}/${id}`;
+    return this.http.get<Pensamento>(url);
+  }
 }
